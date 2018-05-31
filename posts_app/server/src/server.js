0a7445632009819077daa05ba5976e2fbc5c987f
Dario Stamenkovic <stamenkovicdario@gmail.com>
2018-05-31 08:11:00 +0200

    change project structure

--- posts_app/server/src/server.js
+++ posts_app/server/src/server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:41459');
   res.end(actions.createPost());
 });
 
