7b67536082347671a7b88fc9e60003be8a774a3a
BIT Student <classroom@bgit.rs>
2018-07-03 16:39:57 +0200

    add new-recipe feature, delete-recipe feature to recipe_app

--- react_intro/recipe_props_starter/src/Navbar.js
+++ react_intro/recipe_props_starter/src/Navbar.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import './Navbar.css';
 
 class Navbar extends Component {
+    
     render() {
         return (
         <header>
             <h2><a>Recipe App</a></h2>
             <nav>
-                <li><a>New Recipe</a></li>
+                <li><a onClick={this.props.onShowForm}>New Recipe</a></li>
                 <li><a>Home</a></li>
                 <li><a>About</a></li>
                 <li><a>Contact Us</a></li>
